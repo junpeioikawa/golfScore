976be3f0ad0fa0ef2cbed4d50cb32abdba254cfa
IbukiHayashida <hayashiidaibuki@gmail.com>
2024-10-23 20:51:09 +0900

    ゴルフ場マスタとコースマスタを作成

--- golfScore/golfScore_app/models.py
+++ golfScore/golfScore_app/models.py
@@ -29,3 +29,53 @@ class CourseMaster(models.Model):
     create_at = models.DateTimeField(blank=False, null=False)  # 登録日
     update_at = models.DateTimeField(blank=False, null=False)  # 更新日
 
+
+
+
+
+
+# 2024/10/23 hayashida start
+class GolfHouseMaster(models.Model):
+    house_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=True)  # ゴルフ場ID
+    house_name = models.CharField(max_length=255, null=False, blank=False)  # ゴルフ場名
+    house_zipcode = models.CharField(max_length=8, null=False, blank=False)  # 郵便番号
+    house_prefecture = models.CharField(max_length=40, null=False, blank=False)  # 都道府県
+    house_city = models.CharField(max_length=100, null=False, blank=False)  # 市町村区
+    house_address = models.CharField(max_length=255, null=False, blank=False)  # 住所
+    house_phone_number = models.CharField(max_length=11, null=False, blank=False)  # 電話番号
+    create_at = models.DateTimeField(blank=False, null=False)  # 登録日
+    update_at = models.DateTimeField(blank=False, null=False)  # 更新日
+    
+class CorseMaster(models.Model):
+    house_id = models.ForeignKey(GolfHouseMaster, on_delete=models.CASCADE)
+    course_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=True)  # コースID
+    course_name = models.CharField(max_length=255, null=False, blank=False)  # コース名
+    hole_number_1 = models.IntegerField(max_length=2, null=False, blank=False)
+    hole_number_2 = models.IntegerField(max_length=2, null=False, blank=False)
+    hole_number_3 = models.IntegerField(max_length=2, null=False, blank=False)
+    hole_number_4 = models.IntegerField(max_length=2, null=False, blank=False)
+    hole_number_5 = models.IntegerField(max_length=2, null=False, blank=False)
+    hole_number_6 = models.IntegerField(max_length=2, null=False, blank=False)
+    hole_number_7 = models.IntegerField(max_length=2, null=False, blank=False)
+    hole_number_8 = models.IntegerField(max_length=2, null=False, blank=False)
+    hole_number_9 = models.IntegerField(max_length=2, null=False, blank=False)
+    par_1 = models.IntegerField(max_length=2, null=False, blank=False)
+    par_2 = models.IntegerField(max_length=2, null=False, blank=False)
+    par_3 = models.IntegerField(max_length=2, null=False, blank=False)
+    par_4 = models.IntegerField(max_length=2, null=False, blank=False)
+    par_5 = models.IntegerField(max_length=2, null=False, blank=False)
+    par_6 = models.IntegerField(max_length=2, null=False, blank=False)
+    par_7 = models.IntegerField(max_length=2, null=False, blank=False)
+    par_8 = models.IntegerField(max_length=2, null=False, blank=False)
+    par_9 = models.IntegerField(max_length=2, null=False, blank=False)
+    handicap_1 = models.IntegerField(max_length=2, null=False, blank=False)
+    handicap_2 = models.IntegerField(max_length=2, null=False, blank=False)
+    handicap_3 = models.IntegerField(max_length=2, null=False, blank=False)
+    handicap_4 = models.IntegerField(max_length=2, null=False, blank=False)
+    handicap_5 = models.IntegerField(max_length=2, null=False, blank=False)
+    handicap_6 = models.IntegerField(max_length=2, null=False, blank=False)
+    handicap_7 = models.IntegerField(max_length=2, null=False, blank=False)
+    handicap_8 = models.IntegerField(max_length=2, null=False, blank=False)
+    handicap_9 = models.IntegerField(max_length=2, null=False, blank=False)
+    
+# 2024/10/23 hayashida end
