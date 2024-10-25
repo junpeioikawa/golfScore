bfb75a7addd1f5bf00d0f925243a21e598fd26bb
junpei <j-oikawa@gon-dola.com>
2024-10-25 22:44:04 +0900

    旧コースマスターを削除

--- golfScore/golfScore_app/models.py
+++ golfScore/golfScore_app/models.py
@@ -22,16 +22,16 @@ class Score(models.Model):
     def __str__(self):
         return f"{self.player.name} - {self.score} ({self.date})"
 
-class CourseMaster(models.Model):
-    course_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=True)  # コースID
-    course_name = models.CharField(max_length=255, null=False, blank=False)  # ゴルフ場名
-    course_zipcode = models.CharField(max_length=8, null=False, blank=False)  # 郵便番号
-    course_prefecture = models.CharField(max_length=40, null=False, blank=False)  # 都道府県
-    course_city = models.CharField(max_length=100, null=False, blank=False)  # 市町村区
-    course_address = models.CharField(max_length=255, null=False, blank=False)  # 住所
-    course_phone_number = models.CharField(max_length=11, null=False, blank=False)  # 電話番号
-    create_at = models.DateTimeField(blank=False, null=False)  # 登録日
-    update_at = models.DateTimeField(blank=False, null=False)  # 更新日
+# class CourseMaster(models.Model):
+#     course_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=True)  # コースID
+#     course_name = models.CharField(max_length=255, null=False, blank=False)  # ゴルフ場名
+#     course_zipcode = models.CharField(max_length=8, null=False, blank=False)  # 郵便番号
+#     course_prefecture = models.CharField(max_length=40, null=False, blank=False)  # 都道府県
+#     course_city = models.CharField(max_length=100, null=False, blank=False)  # 市町村区
+#     course_address = models.CharField(max_length=255, null=False, blank=False)  # 住所
+#     course_phone_number = models.CharField(max_length=11, null=False, blank=False)  # 電話番号
+#     create_at = models.DateTimeField(blank=False, null=False)  # 登録日
+#     update_at = models.DateTimeField(blank=False, null=False)  # 更新日
 
 
 
