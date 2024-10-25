f050d6cad23ca3084611708dbd4e8dbcdb5c1751
IbukiHayashida <hayashiidaibuki@gmail.com>
2024-10-25 23:54:24 +0900

    admin.py編集

--- golfScore/golfScore_app/admin.py
+++ golfScore/golfScore_app/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Player, Score
+from .models import Player, Score, GolfHouseMaster, CourseMaster, Round
 
 admin.site.register(Player)
 admin.site.register(Score)
+admin.site.register(GolfHouseMaster)
+admin.site.register(CourseMaster)
+admin.site.register(Round)
