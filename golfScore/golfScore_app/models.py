b8679b0bfaee88449dcf579ebd36f74f959ed283
junpei <j-oikawa@gon-dola.com>
2024-10-26 00:04:20 +0900

    管理画面の項目名を日本語にする方法※林田の功績

--- golfScore/golfScore_app/models.py
+++ golfScore/golfScore_app/models.py
@@ -41,13 +41,13 @@ class Score(models.Model):
 
 # 2024/10/23 hayashida start
 class GolfHouseMaster(models.Model):
-    house_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=True)  # ゴルフ場ID
-    house_name = models.CharField(max_length=255, null=False, blank=False)  # ゴルフ場名
-    house_zipcode = models.CharField(max_length=8, null=False, blank=False)  # 郵便番号
-    house_prefecture = models.CharField(max_length=40, null=False, blank=False)  # 都道府県
-    house_city = models.CharField(max_length=100, null=False, blank=False)  # 市町村区
-    house_address = models.CharField(max_length=255, null=False, blank=False)  # 住所
-    house_phone_number = models.CharField(max_length=11, null=False, blank=False)  # 電話番号
+    house_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=True, verbose_name='ゴルフ場ID')  # ゴルフ場ID
+    house_name = models.CharField(max_length=255, null=False, blank=False, verbose_name='ゴルフ場名')  # ゴルフ場名
+    house_zipcode = models.CharField(max_length=8, null=False, blank=False, verbose_name='郵便番号')  # 郵便番号
+    house_prefecture = models.CharField(max_length=40, null=False, blank=False, verbose_name='都道府県')  # 都道府県
+    house_city = models.CharField(max_length=100, null=False, blank=False, verbose_name='市町村区')  # 市町村区
+    house_address = models.CharField(max_length=255, null=False, blank=False, verbose_name='住所')  # 住所
+    house_phone_number = models.CharField(max_length=11, null=False, blank=False, verbose_name='電話番号')  # 電話番号
     create_at = models.DateTimeField(default=timezone.now)  # 登録日
     update_at = models.DateTimeField(auto_now=True)  # 更新日
     
