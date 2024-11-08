6e0ff4532275e0814e5941a3d14986466367204c
IbukiHayashida <hayashiidaibuki@gmail.com>
2024-11-09 00:31:04 +0900

    一旦コミット

--- golfScore/golfScore_app/views/user_views.py
+++ golfScore/golfScore_app/views/user_views.py
@@ -1,6 +1,10 @@
 from django.http.response import HttpResponse
 from django.views.generic import CreateView
 from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LogoutView
+from django.urls import reverse_lazy
+from django.views.generic.edit import CreateView
+from django.contrib.auth.forms import UserCreationForm
 #from golfScore_app.forms import SignUpForm # forms.py未完成
 
 # Create your views here.
@@ -10,13 +14,16 @@ def test(request):
     return HttpResponse('user test')
 
 # アカウント登録処理
-class SignUpView(CreateView):
-    # form_class = SignUpForm
-    success_url = '/login/'
-    template_name = 'user_pages/login_signup.html'
-    
-    def form_valid(self, form):    
-        return super().form_valid(form)
+class UserSignUpView(CreateView):
+    form_class = UserCreationForm
+    template_name = 'user_pages/signup.html'
+    success_url = reverse_lazy('login')
+
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        # ここで追加の処理を行うことができます
+        return response
+
     
 
 # ログイン処理
@@ -25,3 +32,6 @@ class UserLoginView(LoginView):
     def form_valid(self, form):        
         return super().form_valid(form)
     
+# ログアウト処理
+class UserLogoutView(LogoutView):
+    next_page = '/login/'
